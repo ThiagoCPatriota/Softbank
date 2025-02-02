a3145dc7077b465cb3eeb8808c45ff03f5b7835a
Thiago Patriota <thiagosilvapatriota@gmail.com>
2025-02-02 19:59:21 -0300

    Mudança final

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from defs.cadastro_login import *
 
 def main(page :ft.Page):
     page.controls.clear()
-    page.window_width = 1080  
-    page.window_height = 720
+    page.window.width = 1080  
+    page.window.height = 720
+    
 
     page.appbar = ft.AppBar(
-        leading = ft.Icon(ft.icons.ACCOUNT_BALANCE_SHARP),
+        leading = ft.Icon(ft.Icons.ACCOUNT_BALANCE_SHARP),
         title = ft.Text('Softbank'),
         center_title = True,
         bgcolor = '#46295A', #roxo
